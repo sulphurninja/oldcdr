f8bd33a12e7635e961d3790053be4fde0cab5fff
sulphurninja <sulphurninja@gmail.com>
2023-07-19 19:27:09 +0530

    WOW

--- components/UserNameAnalysis.js
+++ components/UserNameAnalysis.js
@@ -3,11 +3,11 @@ import { motion } from 'framer-motion'
 
 export default function UserNameAnalysis() {
   return (
-    <div className='mt-36 ml-[40%]'>
+    <div className='mt-24 md:mt-36 ml-[25%] md:ml-[10%]'>
       <motion.div whileHover={{
         scale: 1.05,
         transition: { duration: 0.2 },
-      }} className='absolute cursor-pointer md:h-[40%] h-[6%]'>
+      }} className='absolute cursor-pointer md:h-[40%] h-[25%]'>
         <img src='username.png' className='h-full w-full ' />
       </motion.div>
     </div>
