5ebdab91a8bf46fa440b5034e1910cd45a924274
sulphurninja <sulphurninja@gmail.com>
2023-07-22 11:46:29 +0530

    WOW

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  env: {
+    "ACCESS_TOKEN_SECRET": "2MqS&V^f_&UudgstXMbnd3LTdC_zkSzBYVB#Q=9%Rjm&fFBW6zTE3^s646kJ",
+    "REFRESH_TOKEN_SECRET": "S%=VY!nj3rkK@?VZ_v?bQVFzhz^rSA#vKgJ4Rb23j_XCjQXU?fY9AmA-MgJWHae9f*Xz%P*x$WaGT*7c8HM67B-R%Z_zcy"
+  }
 }
 
 module.exports = nextConfig
